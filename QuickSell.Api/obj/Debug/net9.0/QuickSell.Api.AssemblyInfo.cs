b1bc0382a9328596a61dbd559c7a720d263c5b5a
Mohammad Abdullah Imran <mohammadabdullahimran@Mohammad-sin-MacBook-Air.local>
2025-07-04 23:40:40 +0200

    React client skeleton added

--- QuickSell.Api/obj/Debug/net9.0/QuickSell.Api.AssemblyInfo.cs
+++ QuickSell.Api/obj/Debug/net9.0/QuickSell.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickSell.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e8c7ddd247dade9112cc371e2d3f8a0be651dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f752c24975b938adb67d7e7da6c519b2b4cdd1f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickSell.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickSell.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ QuickSellClient/Components/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+
+    return(
+        <div className="card-container">
+            <h1></h1>
+            <p></p>
+        </div>
+    );
+}
+
+export default Card;
