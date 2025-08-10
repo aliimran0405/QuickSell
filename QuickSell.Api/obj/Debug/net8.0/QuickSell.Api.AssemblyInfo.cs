923086158903339adee7f092666a457236634718
Mohammad Abdullah Imran <mohammadabdullahimran@Mohammad-sin-MacBook-Air.local>
2025-08-11 01:08:40 +0200

    updated db using azure conn string

--- QuickSell.Api/obj/Debug/net8.0/QuickSell.Api.AssemblyInfo.cs
+++ QuickSell.Api/obj/Debug/net8.0/QuickSell.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickSell.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a116c84b31a467722b9e4c25444d311b057457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0d5af7a8850c5426fc3ef593e0c8c1786bc6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickSell.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickSell.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
